28a14cabee09d6359eca37f2927d0afc364c6177
Sri Ram <ch.sriram999@gmail.com>
2018-02-20 00:23:17 +0000

    restored file from third commit, 6th commit.

--- Helloworld.java
+++ Helloworld.java
@@ -3,8 +3,6 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
 	System.out.println("Hello, World");
-//Add print line.
 	System.out.println("Git Update");
-	System.out.println("Welcome to GIT, This is 5th update");
    }
 }
